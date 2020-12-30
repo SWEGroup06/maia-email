f914ae4d3950e045ed5d1de5bf572e86b5a21f91
Ihowa <ihowaonaro@gmail.com>
2020-12-30 17:03:37 +0100

    Implemented email handler for schedule and reschedule mails

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const CONFIG = require('./config.js');
 
 // Modules
 const express = require('express');
+const axios = require('axios');
 
 // Libraries
 const EMAIL = require('./lib/email/index.js');
@@ -36,6 +37,54 @@ router.post('/webhook', async function(req, res) {
                 await EMAIL.mailer.sendMail(address, 'Maia Logout', `<div>You signed out of the Maia AI Calendar Assistant</div>`);
                 return;
             }
+
+            const title = email.subject;
+            const members = [address].concat(email.to);
+            let body = email.body;
+            body = body.split(/\n/).map(s => s.trim()).filter(s => s.length !== 0);
+            body = body.slice(1, body.length - 2)
+            const res = await axios.get(CONFIG.serverURL + '/nlp/', {
+                params: {
+                    text: encodeURIComponent(JSON.stringify(body.join(' '))),
+                }
+            });
+
+            if (res.type == 'schedule') {
+                await axios.get(CONFIG.serverURL + '/api/schedule', {
+                    params: {
+                        emails: members,
+                        title: title,
+                        flexible: res.data.flexible,
+                        duration: res.data.duration,
+                        startDateTimeOfRange: res.data.startDateTimeOfRange,
+                        endDateTimeOfRange: res.data.endDateTimeOfRange,
+                        beforeAfterKey: res.data.beforeAfterKey,
+                    }
+                });
+
+                await EMAIL.mailer.sendMail(address, `${title} Scheduled`,
+                                  `<div>Your event has been scheduled by Maia for${res.data.startDateTimeOfRange}</div>`);
+                return;
+            }
+
+            if (res.type == 'reschedule') {
+                await axios.get(CONFIG.serverURL + '/api/reschedule', {
+                    params: {
+                        meetingTitle: title,
+                        organiserSlackEmail: address,
+                        eventStartTime: res.data.eventStartTime,
+                        newStartDateTime: res.data.newStartDateTime,
+                        newEndDateTime: res.data.newEndDateTime,
+                        beforeAfterKey: res.data.beforeAfterKey,
+                    }
+                });
+
+                await EMAIL.mailer.sendMail(address, `${title} Re-scheduled`,
+                                `<div>Your event has been re-scheduled by Maia from ${res.data.eventStartTime} to ${res.data.newStartDateTime}</div>`);
+                return;
+            }
+
+
         }
     } catch (err) {
         console.error(err);
