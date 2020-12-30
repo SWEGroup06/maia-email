dcf44b1822287530d5930afdf72dfa3be2a9fb89
KPal81 <kpal81xd@gmail.com>
2020-12-30 17:02:05 +0000

    Added in time delay between webhook and fetching emails

--- routes.js
+++ routes.js
@@ -19,28 +19,32 @@ router.get('/', function(_, res) {
 // Gmail Pub/Sub callback
 router.post('/webhook', async function(req, res) {
     res.sendStatus(200);
-    try {
-        const emails = await EMAIL.webhook.fetch(req.body);
-        for (const email of emails) {
-            const address = email.from.address;
-            if (address == CONFIG.email) continue;
-            if (email.subject == 'LOGIN') {
-                const data = await CONN.login(address);
-                if (data.url) await EMAIL.mailer.sendMail(address, 'Maia Login Link', `<div>Click <a href='${data.url}'>here</a> to login</div>`);
-                else if (data.exists) await EMAIL.mailer.sendMail(address, 'Maia Login Response', `<div>You are already signed in</div>`);
-                else console.log(data);
-                return;
+
+    // Sometimes the email isnt actually in the inbox yet so wait for 500 ms before fetching mail
+    setTimeout(function() {
+        try {
+            const emails = await EMAIL.webhook.fetch(req.body);
+            for (const email of emails) {
+                const address = email.from.address;
+                if (address == CONFIG.email) continue;
+                if (email.subject == 'LOGIN') {
+                    const data = await CONN.login(address);
+                    if (data.url) await EMAIL.mailer.sendMail(address, 'Maia Login Link', `<div>Click <a href='${data.url}'>here</a> to login</div>`);
+                    else if (data.exists) await EMAIL.mailer.sendMail(address, 'Maia Login Response', `<div>You are already signed in</div>`);
+                    else console.log(data);
+                    return;
+                }
+                if (email.subject == 'LOGOUT') {
+                    const data = await CONN.logout(address);
+                    console.log(data);
+                    await EMAIL.mailer.sendMail(address, 'Maia Logout', `<div>You signed out of the Maia AI Calendar Assistant</div>`);
+                    return;
+                }
             }
-            if (email.subject == 'LOGOUT') {
-                const data = await CONN.logout(address);
-                console.log(data);
-                await EMAIL.mailer.sendMail(address, 'Maia Logout', `<div>You signed out of the Maia AI Calendar Assistant</div>`);
-                return;
-            }
+        } catch (err) {
+            console.error(err);
         }
-    } catch (err) {
-        console.error(err);
-    }
+    }, 500);
     
 });
 
