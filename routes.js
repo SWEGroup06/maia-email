e60fd0ff8fd24ca44ed97499d9501ce516dbff4c
KPal81 <kpal81xd@gmail.com>
2021-01-02 16:24:48 +0000

    Implemented schedule, reschedule and cancel commands

--- routes.js
+++ routes.js
@@ -7,6 +7,7 @@ const express = require("express");
 // Libraries
 const EMAIL = require("./lib/email/index.js");
 const CONN = require("./lib/connection.js");
+const COMMANDS = require("./lib/commands.js")(CONN, EMAIL);
 
 // Setup router
 // eslint-disable-next-line new-cap
@@ -29,94 +30,51 @@ router.post("/webhook", function (req, res) {
         const address = email.from.address;
         if (address == CONFIG.email) continue;
 
-        // LOGIN email
-        if (email.subject == "LOGIN") {
-          const data = await CONN.login(address);
-          if (data.url) {
-            await EMAIL.mailer.sendMail(
-              address,
-              "Maia Login Link",
-              `<div>Click <a href='${data.url}'>here</a> to login</div>`
-            );
-          } else if (data.exists) {
-            await EMAIL.mailer.sendMail(
-              address,
-              "Maia Login Response",
-              `<div>You are already signed in</div>`
-            );
-          } else console.log(data);
+        const allEmails = [
+          address,
+          ...email.to.map((em) => em.address),
+          ...(email.cc ? email.cc.map((em) => em.address) : []),
+        ];
+
+        const event = {
+          title: email.subject,
+          organiser: address,
+          members: allEmails.filter(
+            (em) => em != CONFIG.email && em != address
+          ),
+        };
+
+        // Handle specific cases
+        if (["LOGIN", "LOGOUT", "HELP"].includes(email.subject)) {
+          await COMMANDS[email.subject.toLowerCase()].action(event);
           return;
         }
 
-        // LOGOUT email
-        if (email.subject == "LOGOUT") {
-          const data = await CONN.logout(address);
-          console.log(data);
+        // Format body removing first and last lines and concatenating into one line
+        const formatted = email.body
+          .trim()
+          .replace(/\n\n/g, "\n")
+          .replace(/\n\s\n/g, "\n\n");
+
+        const text = formatted.includes("\n\n")
+          ? formatted.split("\n\n").slice(1, -1).join(" ")
+          : formatted;
+
+        // Use NLP to determine command
+        const res = await CONN.nlp(text);
+        if (res.error) throw new Error(res.error);
+        if (res.type === "unknown") {
+          console.log("Invalid Command");
           await EMAIL.mailer.sendMail(
             address,
-            "Maia Logout",
-            `<div>You signed out of the Maia AI Calendar Assistant</div>`
+            "Maia Calendar: Error",
+            `<div>${res.msg}</div><br>
+            <div>The command "${text}" could not be recognised</div>`
           );
           return;
         }
-
-        // Else use NLP to determine command
-
-        // const title = email.subject;
-        // const members = [address].concat(email.to);
-
-        // Format body removing first and last lines and concatenating into one line
-
-        console.log({ body: email.body });
-
-        const text = email.body
-          .trim()
-          .replace(/\n\n/g, "\n")
-          .replace(/\n\s\n/g, "\n\n")
-          .split("\n\n")
-          .slice(1, -1)
-          .join(" ");
-
-        console.log({ text });
-
-        const res = await CONN.nlp(text);
-
-        console.log({ res });
-
-        // if (res.type == 'schedule') {
-        //     await axios.get(CONFIG.serverURL + '/api/schedule', {
-        //         params: {
-        //             emails: members,
-        //             title: title,
-        //             flexible: res.data.flexible,
-        //             duration: res.data.duration,
-        //             startDateTimeOfRange: res.data.startDateTimeOfRange,
-        //             endDateTimeOfRange: res.data.endDateTimeOfRange,
-        //             beforeAfterKey: res.data.beforeAfterKey,
-        //         }
-        //     });
-
-        //     await EMAIL.mailer.sendMail(address, `${title} Scheduled`,
-        //                       `<div>Your event has been scheduled by Maia for${res.data.startDateTimeOfRange}</div>`);
-        //     return;
-        // }
-
-        // if (res.type == 'reschedule') {
-        //     await axios.get(CONFIG.serverURL + '/api/reschedule', {
-        //         params: {
-        //             meetingTitle: title,
-        //             organiserSlackEmail: address,
-        //             eventStartTime: res.data.eventStartTime,
-        //             newStartDateTime: res.data.newStartDateTime,
-        //             newEndDateTime: res.data.newEndDateTime,
-        //             beforeAfterKey: res.data.beforeAfterKey,
-        //         }
-        //     });
-
-        //     await EMAIL.mailer.sendMail(address, `${title} Re-scheduled`,
-        //                     `<div>Your event has been re-scheduled by Maia from ${res.data.eventStartTime} to ${res.data.newStartDateTime}</div>`);
-        //     return;
-        // }
+        const cmd = COMMANDS[res.type];
+        if (cmd && cmd.action) await cmd.action(event, res);
       }
     } catch (error) {
       console.error(error);
