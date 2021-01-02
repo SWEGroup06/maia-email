b62a0084b68b243fdbba96bc02e35cc321288590
KPal81 <kpal81xd@gmail.com>
2021-01-02 20:43:41 +0000

    Added retry command for when no emails are found

--- config.js
+++ config.js
@@ -5,4 +5,5 @@ module.exports = {
   url: DEBUG ? "http://localhost:3000" : "https://maia-email.herokuapp.com",
   serverURL: "https://maia-server.herokuapp.com",
   email: "maiacalendar123@gmail.com",
+  MAX_RETRIES: 5,
 };
